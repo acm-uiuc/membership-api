dad9196599a338f9bbcc26a789e35cfc625d58f5
Dev Singh <dsingh14@illinois.edu>
2024-08-04 03:32:35 -0400

    update tests

--- tests/live_integration/test_checkMembership.py
+++ tests/live_integration/test_checkMembership.py
@@ -2,11 +2,18 @@ def test_valid_member(api_client):
     response = api_client.get("/api/v1/checkMembership?netId=dsingh14")
     assert response.status_code == 200
     assert response.json() == {"netId": "dsingh14", "isPaidMember": True}
+    assert response.headers['x-acm-membership-source']  # it should at least exist, may or may not be cached
+    response = api_client.get("/api/v1/checkMembership?netId=dsingh14")
+    assert response.status_code == 200
+    assert response.json() == {"netId": "dsingh14", "isPaidMember": True}
+    assert response.headers['x-acm-membership-source'] == 'dynamo'
 
 def test_invalid_member(api_client):
     response = api_client.get("/api/v1/checkMembership?netId=invalid")
     assert response.status_code == 200
     assert response.json() == {"netId": "invalid", "isPaidMember": False,}
+    assert response.headers['x-acm-membership-source'] == 'aad' # non-member results should always be double checked against aad
+
 
 def test_invalid_parameters(api_client):
     response = api_client.get("/api/v1/checkMembership")
